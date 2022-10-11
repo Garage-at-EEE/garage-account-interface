ddb28ed5ec59a82ef99dda95cbbec2cbc14864cc
SpiderPigzz <Ricky.winarko@gmail.com>
2022-10-11 23:28:38 +0800

    added portfinder

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -3,6 +3,7 @@ const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const portFinderSync = require('portfinder-sync')
 
 module.exports = merge(common, {
   mode: 'development',
