83db7693ef705d524928a66534afb47511299086
SpiderPigzz <Ricky.winarko@gmail.com>
2022-10-11 23:30:45 +0800

    Update webpack.config.dev.js

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -9,14 +9,36 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
   devServer: {
-    inline: true,
     host: 'local-ip',
-            port: portFinderSync.getPort(6969),
-            open: true,
-            https: false,
-            allowedHosts: 'all',
-            hot: false,
-            watchFiles: ['src/**', 'static/**'],
+    port: portFinderSync.getPort(6969),
+    open: true,
+    https: false,
+    allowedHosts: 'all',
+    hot: false,
+    watchFiles: ['src/**', 'static/**'],
+    static:
+    {
+      watch: true,
+      directory: path.join(__dirname, '../static')
+    },
+    client:
+    {
+      logging: 'none',
+      overlay: true,
+      progress: false
+    },
+    setupMiddlewares: function (middlewares, devServer) {
+      console.log('------------------------------------------------------------')
+      console.log(devServer.options.host)
+      const port = devServer.options.port
+      const https = devServer.options.https ? 's' : ''
+      const domain1 = `http${https}://${devServer.options.host}:${port}`
+      const domain2 = `http${https}://localhost:${port}`
+
+      console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+
+      return middlewares
+    }
   },
   plugins: [
     new Webpack.DefinePlugin({
